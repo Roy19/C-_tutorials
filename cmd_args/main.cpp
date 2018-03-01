e5a4ee06461735a61cec4a8e7a37120a9dcffa65
Aritra Sur Roy <aritra1997@hotmail.com>
2018-03-01 17:42:25 +0530

    Command Line Arguments

---
+++ cmd_args/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc,char *argv[]){
+	
+	if(argc < 6){
+		std::cerr << "Invalid number of arguments, atleast 5 required" << std::endl;
+		return 1;
+	}
+	std::cout << "Total arguments:" << argc << std::endl;
+	for(int i = 0;i < argc;++i){
+		std::cout << "arg[" << i << "] = " << argv[i] <<std::endl;
+	}
+	return 0;
+}
